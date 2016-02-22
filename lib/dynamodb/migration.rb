0432817ac8d2110d5ca29b494d82520042bffd0f
Peak Xu <peak.xu@gmail.com>
2016-02-22 13:04:36 -0500

    Refactor module methods into self class

--- lib/dynamodb/migration.rb
+++ lib/dynamodb/migration.rb
@@ -4,20 +4,22 @@ require "dynamodb/migration/unit"
 
 module DynamoDB
   module Migration
-    def self.registered(app)
-      options = {
-        client: app.dynamodb_client,
-        path:   app.settings.migrations_path,
-      }
-      run_all_migrations(options)
-    end
-
-    def self.run_all_migrations(options)
-      Dir.glob("#{options[:path]}/**/*.rb").each do |file|
-        require file
+    class << self
+      def registered(app)
+        options = {
+          client: app.dynamodb_client,
+          path:   app.settings.migrations_path,
+        }
+        run_all_migrations(options)
       end
-      Execute.new(options[:client])
-             .update_all
+
+      def run_all_migrations(options)
+        Dir.glob("#{options[:path]}/**/*.rb").each do |file|
+          require file
+        end
+        Execute.new(options[:client])
+               .update_all
+      end
     end
   end
 end
